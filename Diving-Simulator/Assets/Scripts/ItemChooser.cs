0257124a993653bcf50e7db89e55e6c861e20d26
ffichant <ffichant@enseirb-matmeca.fr>
2017-03-08 20:56:18 +0100

    Ajout d'un loader (A, Z, E pour passer entre les scènes) Ajout de deux scènes supplémentaires : OnBoat et Score Début de la scène OnBoat: ajout d'un background et de quelques objets qui disparaissent quand on clique dessus Remarque: Attention les images ne sont pas toutes libres de droit

---
+++ Diving-Simulator/Assets/Scripts/ItemChooser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemChooser : MonoBehaviour
+{
+
+    public void unloadOnClick()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Diving-Simulator/Assets/Scripts/SceneLoading.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class SceneLoading : MonoBehaviour {
+
+    public int loadedSceneID = 0;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);//Has to remain between scenes
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Gérer les inputs du joueur
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Sélection de la scène 1");
+            unloadCurrentScene();
+            loadedSceneID = 1;
+            SceneManager.LoadScene("OnBoat", LoadSceneMode.Additive);
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Sélection de la scène 2");
+            unloadCurrentScene();
+            loadedSceneID = 2;
+            SceneManager.LoadScene("Underwater", LoadSceneMode.Additive);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Sélection de la scène 3");
+            unloadCurrentScene();
+            loadedSceneID = 3;
+            SceneManager.LoadScene("Score", LoadSceneMode.Additive);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            //quit game
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif      
+        }
+    }
+
+    void unloadCurrentScene()
+    {
+        if (loadedSceneID == 1)
+        {
+            SceneManager.UnloadSceneAsync("OnBoat");
+            loadedSceneID = 0;
+        }
+        else if (loadedSceneID == 2)
+        {
+            SceneManager.UnloadSceneAsync("Underwater");
+            loadedSceneID = 0;
+        }
+        else if (loadedSceneID == 3)
+        {
+            SceneManager.UnloadSceneAsync("Score");
+            loadedSceneID = 0;
+        }
+    }
+}
