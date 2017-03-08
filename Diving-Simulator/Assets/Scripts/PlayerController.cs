a26ec310ffb327a8e686295448841a1710011ed8
ffichant <ffichant@enseirb-matmeca.fr>
2017-03-08 22:17:38 +0100

    Ajout d'une barre d'oxygène pour la phase en plongée, qui décroit au fil du temps Ajout d'une barre de stamina, pas fonctionnelle pour l'instant

--- Diving-Simulator/Assets/Scripts/PlayerController.cs
+++ Diving-Simulator/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
+    public GameObject oxygenBar;
+    public GameObject palmingBar;
+    public float oxygenDecayRate = 0.02f;
     public float horizontalSpeed = 5.0f;
     public float verticalSpeed = 5.0f;
 
@@ -12,6 +16,13 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.P) && (palmingBar.GetComponent<Scrollbar>().size > 0))
+        {
+            palmingBar.GetComponent<Scrollbar>().size -= Time.deltaTime * oxygenDecayRate;
+        }
+        else if (palmingBar.GetComponent<Scrollbar>().size < 1 && !Input.GetKeyDown(KeyCode.P))
+            palmingBar.GetComponent<Scrollbar>().size += 0.05f;
+
         //Déplacement du joueur
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * horizontalSpeed;
         var y = Input.GetAxis("Vertical") * Time.deltaTime * verticalSpeed;
@@ -23,9 +34,11 @@ public class PlayerController : MonoBehaviour {
 
 
         //Prise en compte de l'oscillation de la position dans l'eau
-        var deltaX = Time.deltaTime*horizontalSpeed*Mathf.Cos(Time.realtimeSinceStartup) * horizontalOscillationCoeff;
+        //var deltaX = Time.deltaTime*horizontalSpeed*Mathf.Cos(Time.realtimeSinceStartup) * horizontalOscillationCoeff;
         var deltaY = Time.deltaTime*verticalSpeed*Mathf.Cos(Time.realtimeSinceStartup) * verticalOscillationCoeff;
-        transform.Translate(x+deltaX, y+deltaY, 0);
+        transform.Translate(x, y+deltaY, 0);
 
+
+        oxygenBar.GetComponent<Scrollbar>().size -= Time.deltaTime * oxygenDecayRate;
     }
 }
