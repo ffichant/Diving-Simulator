4510d4d113388698db0dd9532e63a443e4c1415a
ffichant <ffichant@enseirb-matmeca.fr>
2017-03-08 19:10:03 +0100

    Ajout d'un mouvement d'oscillation pour le joueur, paramètres modifiables Déplacement de la caméra en suivant le joueur

--- Diving-Simulator/Assets/Scripts/PlayerController.cs
+++ Diving-Simulator/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float horizontalSpeed = 10.0f;
-    public float verticalSpeed = 10.0f;
+    public float horizontalSpeed = 5.0f;
+    public float verticalSpeed = 5.0f;
+
+    public float horizontalOscillationCoeff = 0.8f;
+    public float verticalOscillationCoeff = 0f;
     // Update is called once per frame
     void Update()
     {
+        //Déplacement du joueur
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * horizontalSpeed;
         var y = Input.GetAxis("Vertical") * Time.deltaTime * verticalSpeed;
 
@@ -17,7 +21,11 @@ public class PlayerController : MonoBehaviour {
         else if(x > 0)
             GetComponent<Animator>().SetTrigger("TurningRight");
 
-        transform.Translate(x, y, 0);
+
+        //Prise en compte de l'oscillation de la position dans l'eau
+        var deltaX = Time.deltaTime*horizontalSpeed*Mathf.Cos(Time.realtimeSinceStartup) * horizontalOscillationCoeff;
+        var deltaY = Time.deltaTime*verticalSpeed*Mathf.Cos(Time.realtimeSinceStartup) * verticalOscillationCoeff;
+        transform.Translate(x+deltaX, y+deltaY, 0);
 
     }
 }
