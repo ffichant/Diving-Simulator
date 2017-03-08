1f56065be70e0cf730bf45ead74a80e8abb26dbf
ffichant <ffichant@enseirb-matmeca.fr>
2017-03-08 04:41:17 +0100

    Premier commit Ajout d'un background Ajout du joueur et d'un contrôle clavier

---
+++ Diving-Simulator/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float horizontalSpeed = 10.0f;
+    public float verticalSpeed = 10.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * horizontalSpeed;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * verticalSpeed;
+
+        if(x < 0)
+            GetComponent<Animator>().SetTrigger("TurningLeft");
+        else if(x > 0)
+            GetComponent<Animator>().SetTrigger("TurningRight");
+
+        transform.Translate(x, y, 0);
+
+    }
+}
