6c682be130fe301d3c194ab71578dfb4fbf5b155
ffichant <ffichant@enseirb-matmeca.fr>
2017-03-09 02:43:30 +0100

    v0.1.1 : Version de la présentation de l'alpha

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour {
     public GameObject oxygenBar;
     public GameObject palmingBar;
     public float oxygenDecayRate;
+    public float staminaUseRate;
+    public float staminaRegenerationRate;
+    public float horizontalStaminaSpeedBoost;//Représente un coefficient multiplicateur
     public float horizontalSpeed;
     public float verticalSpeed;
     public float verticalOscillationCoeff;
@@ -15,17 +18,21 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P) && (palmingBar.GetComponent<Scrollbar>().size > 0))
+        //Utilisation de la stamina
+        var deltaStamina = 1.0f;
+        if (Input.GetKey(KeyCode.Space) && (palmingBar.GetComponent<Scrollbar>().size > 0))
         {
-            palmingBar.GetComponent<Scrollbar>().size -= Time.deltaTime * oxygenDecayRate;
+            palmingBar.GetComponent<Scrollbar>().size -= Time.deltaTime * staminaUseRate;
+            deltaStamina += horizontalStaminaSpeedBoost;
         }
-        else if (palmingBar.GetComponent<Scrollbar>().size < 1 && !Input.GetKeyDown(KeyCode.P))
-            palmingBar.GetComponent<Scrollbar>().size += 0.05f;
+        else if (palmingBar.GetComponent<Scrollbar>().size < 1 && !Input.GetKey(KeyCode.Space))
+            palmingBar.GetComponent<Scrollbar>().size += staminaRegenerationRate;
 
         //Déplacement du joueur
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * horizontalSpeed;
         var y = Input.GetAxis("Vertical") * Time.deltaTime * verticalSpeed;
 
+        //Animation du joueur
         if(x < 0)
             GetComponent<Animator>().SetTrigger("TurningLeft");
         else if(x > 0)
@@ -33,9 +40,8 @@ public class PlayerController : MonoBehaviour {
 
 
         //Prise en compte de l'oscillation de la position dans l'eau
-        //var deltaX = Time.deltaTime*horizontalSpeed*Mathf.Cos(Time.realtimeSinceStartup) * horizontalOscillationCoeff;
         var deltaY = Time.deltaTime*verticalSpeed*Mathf.Cos(Time.realtimeSinceStartup) * verticalOscillationCoeff;
-        transform.Translate(x, y+deltaY, 0);
+        transform.Translate(x*deltaStamina, y+deltaY, 0);
 
 
         oxygenBar.GetComponent<Scrollbar>().size -= Time.deltaTime * oxygenDecayRate;
