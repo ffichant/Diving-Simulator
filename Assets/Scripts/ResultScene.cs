fce73731f55e9979e2235894f2556abbd55972f4
ArokThar <jeremie93@laposte.net>
2017-04-26 18:17:47 +0200

    Score ajout

---
+++ Assets/Scripts/ResultScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScene : MonoBehaviour {
+
+	public GameObject textBoxError;
+	public GameObject textBoxScore;
+	public SceneLoading sceneLoading;
+	public ScoreManager scoreManager;
+
+	void Start ()
+	{
+		sceneLoading = GameObject.FindWithTag ("SceneManager").GetComponent<SceneLoading> ();
+		scoreManager = GameObject.FindWithTag ("ScoreManager").GetComponent<ScoreManager> ();
+		afficherScore ();
+	}
+
+	public void afficherScore()
+	{
+		string score = "";
+		string scoreTotal = "Score Total : \t\t\t\t\t\t\t";
+		string tmp = scoreManager.getAllErrorPrep();
+		score += tmp;
+		tmp = scoreManager.getAllErrorDive();
+		score += tmp;
+		textBoxError.GetComponent<Text> ().text = score;
+		textBoxScore.GetComponent<Text> ().text = scoreManager.currentScore + "%";
+	}
+
+	public void loadScene(string sceneName)
+	{
+		sceneLoading.loadScene (sceneName);
+	}
+}
