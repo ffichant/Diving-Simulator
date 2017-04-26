f2316f4d7e1b71ff7706c4548ceefd704750df89
Chrono73 <mlocqueville@zen-conseil.com>
2017-04-26 07:02:32 -0400

    Début de l'implémentation des signes

---
+++ Assets/Scripts/Underwater/SignLanguageManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignLanguageManager : MonoBehaviour {
+
+    public Image Instructor;
+    public Button Player1st;
+    public Button Player2nd;
+    public Button Player3rd;
+
+    public GameObject InstructorPanel;
+    public GameObject PlayerPanel;
+
+    public Sprite Sign_Ok;
+    public Sprite Sign_HalfGauge;
+    public Sprite Sign_Reserve;
+    public Sprite Sign_Manometer;
+    public Sprite Sign_Up;
+    public Sprite Sign_Stop;
+    public Sprite Sign_No;
+
+    public bool WaitingForTrigger;
+
+    private void Start(){
+
+        Instructor = Instructor.GetComponent<Image>();
+        InstructorPanel.SetActive(false);
+
+        Player1st = Player1st.GetComponent<Button>();
+        Player2nd = Player2nd.GetComponent<Button>();
+        Player3rd = Player3rd.GetComponent<Button>();
+        PlayerPanel.SetActive(false);
+
+        WaitingForTrigger = false;
+    }
+
+    public void SetupFirstQuiz(){
+
+        InstructorPanel.SetActive(true);
+        Instructor.sprite = Sign_Ok;
+
+        WaitingForTrigger = true;
+
+        /*
+        Player1st.image.sprite = Sign_Up;
+        Player2nd.image.sprite = Sign_Ok;
+        Player3rd.image.sprite = Sign_Stop;
+        */
+    }
+
+}
